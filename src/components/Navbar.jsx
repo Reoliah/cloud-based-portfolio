3a29e8d9b14a59a10db96a5c2a9c5215122f126e
Reoliah <tenidaisy@gmail.com>
2026-06-23 19:22:31 +0100

    typo error in imports fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,13 +49,13 @@ export default function Navbar({ isDarkMode, toggleTheme }) {
                 <ul className="flex flex-col">
                   {navLinks.map((link) => (
                     <li key={link}>
-                      <a
+                      <Link
                         href={link === "Home" ? "/" : `/${link.toLowerCase()}`}
                         onClick={() => setIsOpen(false)} // Close menu when clicked
-                        className="block px-6 -py-1 font-header font-bold uppercase text-[30px] text-copy-light dark:text-copy-dark hover:text-primary-light dark:hover:text-primary-dark transition-colors"
+                        className="block px-6 py-2 font-header font-bold uppercase text-[30px] text-copy-light dark:text-copy-dark hover:text-primary-light dark:hover:text-primary-dark transition-colors"
                       >
                         {link}
-                      </a>
+                      </Link>
                     </li>
                   ))}
                 </ul>
